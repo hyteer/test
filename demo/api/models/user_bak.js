e86ce5bb307ab19ed2baea8b49c42b3132b8b63c
hyteer <hyteer@qq.com>
2016-01-24 23:16:10 +0800

    some changes of handlebars

---
+++ demo/api/models/user_bak.js
@@ -0,0 +1,58 @@
+//user.js
+var  mongodb = require('mongodb');
+var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+var  db = new mongodb.Db('api', server, {safe:true});
+
+//var mongodb = require('./db'); 
+function User(user) { 
+    this.name = user.name; 
+    this.password = user.password; 
+  }; 
+module.exports = User; 
+
+User.prototype.save =  function  save(callback) {   // Store to Mongodb 
+    var  user = {     name:  this.name,     password:  this.password,   };   
+    db.open(function(err, db) { 
+        if (err)  { 
+            return  callback(err);     }     // Read users collection     
+        db.collection('users', function(err, collection) { 
+            if  (err) { 
+                mongodb.close(); 
+                return  callback(err);       } 
+              // Add index for name property
+              collection.ensureIndex('name', {unique:  true});
+               // Read  user  doc
+                collection.insert(user, {safe:  true},  function(err, user) { 
+                     mongodb.close();
+                     callback(err, user);
+               });
+         }); 
+   }); 
+  }; 
+
+User.get =  function  get(username, callback) {
+   db.open(function(err, db) {
+     if (err) { return  callback(err);
+     } 
+    // Read users collection
+     db.collection('users', function(err, collection) {
+         if  (err) {
+             mongodb.close();
+             return  callback(err); 
+          } 
+      // Find doc where name  property is username 
+      collection.findOne({name: username},  function(err, doc) {
+         mongodb.close();
+         if  (doc) {
+           // 封装文档为 User 对象
+         var  user =  new  User(doc);
+		}
+	  else{
+           callback(err, user); 
+               } 
+             
+      });
+     });
+   }); 
+}; 
+
