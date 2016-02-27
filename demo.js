b05cec364988e554a16648c27f7a8791babf5bf9
hyteer <hyteer@qq.com>
2016-02-27 12:00:57 +0800

    socket.io&chat demo

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ var express=require('express');
 var app=express();
 
 app.set('port',process.env.PORT || 80);
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/demo'));
 
 
 hp.createServer(app).listen(app.get('port'),function(){
---
+++ test.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var routes = require('./routes');
+var ng=0;
+var ns=0;
+
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/pub'))
+
+app.get('/', routes.index);
+app.get('/yt', routes.yt);
+  
+  
+ 
+var users={'silly':{name:'Yt',sex:'male'}};
+app.all('/user/:username',function(req,res,next){
+    if(users[req.params.username]){
+    ng+=1;
+    console.log("msg recieved: "+ng);
+    next();}
+    else{
+    next(new Error(req.params.username+' does not exist.'));}
+});
+app.get('/user/:username',function(req,res){
+    res.send(JSON.stringify(users[req.params.username])); 
+    ns+=1;
+    console.log("msg sent: "+ns);
+});
+
+
+app.listen(3000);
+console.log('Node test server listen on:3000');
